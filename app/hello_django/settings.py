c59730e3843aeed4bd165b87a8f92181cf9b20ee
Jayavardhan Kunadi <jayavardhankunadi@gmail.com>
2024-06-23 20:42:24 +0530

    comma changes

--- app/hello_django/settings.py
+++ app/hello_django/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
     'default': {
         'ENGINE': os.environ.get("SQL_ENGINE",'django.db.backends.sqlite3'),
         'NAME': os.environ.get("SQL_DATABASE",BASE_DIR / 'db.sqlite3'),
-        'USER':os.environ.get("SQL_USER","user")
-        'PASSWORD':os.environ.get("SQL_PASSWORD","password")
-        'HOST':os.environ.get("SQL_HOST",'localhost')
+        'USER':os.environ.get("SQL_USER","user"),
+        'PASSWORD':os.environ.get("SQL_PASSWORD","password"),
+        'HOST':os.environ.get("SQL_HOST",'localhost'),
         'PORT':os.environ.get("SQL_PORT","5432")
     }
 }
