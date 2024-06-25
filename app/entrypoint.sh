383eae154fb8fa09e4f5bb5c168a24504e314687
Jayavardhan Kunadi <jayavardhankunadi@gmail.com>
2024-06-26 00:32:25 +0530

    c5

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$DATABASE" = "postgres"]
 then
     echo "Postgres is Waiting"
 
-    while ! nc -z $SQL_HOST $SQL_PORT do
+    while ! nc -z $SQL_HOST $SQL_PORT;do
         sleep 0.1
     done
 
