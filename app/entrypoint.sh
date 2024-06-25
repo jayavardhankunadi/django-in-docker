dda3aa5bbe8bb57f7e62f5ce8877627f00951adf
Jayavardhan Kunadi <jayavardhankunadi@gmail.com>
2024-06-25 23:40:04 +0530

    changes

---
+++ app/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres"]
+then
+    echo "Postgres is Waiting"
+
+    while ! nc -z $SQL_HOST $SQL_PORT do
+        sleep 0.1
+    done
+
+    echo "Postgres is started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
+exec "$@"
